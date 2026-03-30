8242092e080f8b3532937c1c2c5c1007c902b9ba
rob587 <robertocamm02@yahoo.com>
2026-03-30 14:41:51 +0200

    feat: implement product listing with FlatList and add sample data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,29 @@
-import { Text, View } from "react-native";
+import prodotti from "@/data/prodotti";
+import { router } from "expo-router";
+import { FlatList, Pressable, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="flex-1 bg-white">
+      <FlatList
+        data={prodotti}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Pressable
+            className="flex-row items-center justify-between p-4 mb-3 rounded-xl bg-gray-100"
+            onPress={() =>
+              router.push(
+                `/dettaglio?id=${item.id}&nome=${item.nome}&prezzo=${item.prezzo}`,
+              )
+            }
+          >
+            <Text className="text-lg font-bold">{item.nome}</Text>
+            <Text className="text-base text-indigo-600">{item.prezzo}</Text>
+            <Text className="text-sm text-gray-500">{item.categoria}</Text>
+            <Text></Text>
+          </Pressable>
+        )}
+      ></FlatList>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
---
+++ data/prodotti.ts
@@ -0,0 +1,29 @@
+const prodotti = [
+  { id: "1", nome: "iPhone 15", prezzo: "999€", categoria: "Smartphone" },
+  { id: "2", nome: "MacBook Air M2", prezzo: "1299€", categoria: "Laptop" },
+  { id: "3", nome: "AirPods Pro", prezzo: "279€", categoria: "Audio" },
+  {
+    id: "4",
+    nome: "Samsung Galaxy S24",
+    prezzo: "899€",
+    categoria: "Smartphone",
+  },
+  { id: "5", nome: "Sony WH-1000XM5", prezzo: "349€", categoria: "Audio" },
+  { id: "6", nome: "iPad Pro 12.9", prezzo: "1199€", categoria: "Tablet" },
+  { id: "7", nome: "Dell XPS 15", prezzo: "1799€", categoria: "Laptop" },
+  {
+    id: "8",
+    nome: "Apple Watch Series 9",
+    prezzo: "449€",
+    categoria: "Wearable",
+  },
+  { id: "9", nome: "GoPro Hero 12", prezzo: "399€", categoria: "Camera" },
+  {
+    id: "10",
+    nome: "Kindle Paperwhite",
+    prezzo: "159€",
+    categoria: "E-Reader",
+  },
+];
+
+export default prodotti;
